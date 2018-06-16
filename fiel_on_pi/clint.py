357162a8268788a6efb4e21988550e17f3b7eff4
cantstumpthetrump2000 <camerondrain@gmail.com>
2018-06-16 07:43:59 +0100

    -

--- fiel_on_pi/clint.py
+++ fiel_on_pi/clint.py
@@ -17,4 +17,31 @@ while chunk:
 sock.close()
 
 
-print(holad)
+
+import matplotlib.pyplot as plt
+
+
+
+x_points=[]
+y_points=[]
+
+step1=holad
+
+
+step2=step1.split("\n")
+
+for q in step2[0:-1]:
+    print(q)
+    y,x=q.split("-")
+    x_points.append(float(x))
+    y_points.append(float(y))
+
+
+
+
+
+
+plt.plot(x_points,y_points)
+plt.ylabel('some numbers')
+plt.xlabel('some numbers')
+plt.show()
