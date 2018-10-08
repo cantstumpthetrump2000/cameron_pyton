d911739ae0a3a48f29b89ea4e5cc8ab8fbda894a
cantstumpthetrump2000 <camerondrain@gmail.com>
2018-10-08 09:44:19 +0100

    update

--- python_scokets/sevr_cllint loacal coneection/clinet.py
+++ python_scokets/sevr_cllint loacal coneection/clinet.py
@@ -2,23 +2,17 @@ import socket
 
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = '127.0.0.1'
-port = 50008
+host = '127.16.10.1'
+
+port =27036
+print("runign")
 
 s.connect((host,port))
 
-def ts(data_to_send):
 
-   data_to_send=str(data_to_send)
+data_from_sever = s.recv(1024).decode()
+print ("data from server ",data_from_sever)
 
 
-   s.send(data_to_send.encode())
-
-   data_from_sever = s.recv(1024).decode()
-   print (data_from_sever)
-
-while 1:
-   r = input('enter')
-   ts(r)
 
 s.close ()
